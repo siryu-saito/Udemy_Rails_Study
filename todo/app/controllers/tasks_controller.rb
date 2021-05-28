29549bf87464fca008b167a2ef7aeb6e042b5da0
siryu-saito <saito.s.ryu@gmail.com>
2021-05-28 05:02:02 +0000

    [add]title validates

--- todo/app/controllers/tasks_controller.rb
+++ todo/app/controllers/tasks_controller.rb
@@ -2,21 +2,21 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
-  
+
   def new
     @task = Task.new
   end
-  
+
   def create
     @task = Task.new(task_params)
-    
+
     if @task.save
       redirect_to root_path
     else
-      render ''
+      render 'new'
     end
   end
-  
+
   private
   def task_params
     params[:task].permit(:title)
